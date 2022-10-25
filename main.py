feebd10e34def9b95361488a046487a7c99b53ba
JAE KYUNG <115970361+bmr03016@users.noreply.github.com>
2022-10-25 12:28:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import replit
 import hangman_art 
 import hangman_words
 
-print(hangman_art.logo)
+print(hangman_art.logo) # 첫 시작 로고 불러오기
 
 chosen_word = random.choice(hangman_words.word_list) 
 word_length = len(chosen_word)
@@ -19,16 +19,19 @@ for _ in range(word_length) :
   display += "_"
 
 while not end_of_game: 
-  guess = input("글자를 맞춰보세요: ").lower() replit.clear()
+  guess = input("글자를 맞춰보세요: ").lower() 
+  replit.clear()
 
 if guess in display: 
   print(f"이미 {guess}를 입력했습니다.")
 
+# 고른 알파벳이 있는 경우 -> letter 작성  
 for position in range(word_length) : 
   letter = chosen_word[position] 
   if letter == guess: 
     display[position] = letter
 
+# 고른 알파벳이 없는 경우 -> 목숨 하나 없어짐
 if guess not in chosen_word: 
   print(f"당신이 고른 {guess}는 단어에 없습니다.\n You lose a life") 
   lives -= 1 
@@ -37,6 +40,7 @@ if guess not in chosen_word:
 
 print(f"{' '.join(display)}")
 
+# 알파벳 다 맞춘경우 -> _ 이 다 사라질 때 -> 이김
 if "_" not in display: 
   end_of_game = True 
   print("You Win!")
